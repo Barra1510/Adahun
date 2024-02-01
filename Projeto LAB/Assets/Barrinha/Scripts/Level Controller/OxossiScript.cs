83e55d3f3df5109d629f428bc5b9fb65fa86782b
Barra1510 <72721117+Barra1510@users.noreply.github.com>
2024-02-01 11:13:39 -0300

    Bugs resolvidos
    Bug pulo - resolvido
    Trocar de fase - resolvido
    Obi dourado dialogo - já feito

--- Projeto LAB/Assets/Barrinha/Scripts/Level Controller/OxossiScript.cs
+++ Projeto LAB/Assets/Barrinha/Scripts/Level Controller/OxossiScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OxossiScript : MonoBehaviour
 {
@@ -16,8 +17,9 @@ public class OxossiScript : MonoBehaviour
 
     public void Fase2()
     {
-        sm = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        sm = GameObject.Find("SoundManager").GetComponent<SoundManager>();        
         sm.Stop("Fase1");
-        sm.Play("Fase2");       
+        SceneManager.LoadScene("LVL2");
+
     }
 }
